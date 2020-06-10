5eec3bde539410d7430866e421732f9b7864dd16
Marcel van den Dungen <marcel@van-den-dungen.com>
2020-06-09 22:54:24 -0700

    Recalc on checkbox click

--- site.js
+++ site.js
@@ -1,68 +1,73 @@
 (
     async () => {
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        let incomes = [];
-        for(var i = 0; i < 12; i++) {
-            incomes.push(0);
-        }
-        let expenses = []
-        for(var i = 0; i < 12; i++) {
-            expenses.push(0);
-        }
-        let categories = new Set();
+        let incomes = new Array(12).fill(0);
+        let expenses = new Array(12).fill(0);
+        let categoriesSet = new Set();
 
-        var apiData = await getApiData();
-        apiData.map(d => processTransaction(d));
-
-        addCheckboxes(Array.from(categories));
+        var transactions = await getTransactionData();
+        transactions.map(d => processTransaction(d));
 
         const button = document.getElementById("refreshButton");
         button.addEventListener("click", refresh);
 
+        let categories = Array.from(categoriesSet);
+
+        addFilters(categories);
+
         const ctx = document.getElementById('myChart');
-        const chart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: months,
-                datasets: [{
-                    label: 'income',
-                    data: incomes,
-                    backgroundColor: '#0A0',
-                    borderWidth: 2,
-                    borderColor: '#777',
-                    hoverBorderWidth: 3,
-                    hoverBorderColor: '#000',
-                    hoverBackgroundColor: '#0A0'
-                    },
-                    {
-                    label: 'expenses',
-                    data: expenses,
-                    backgroundColor: '#D00',
-                    borderWidth: 2,
-                    borderColor: '#777',
-                    hoverBorderWidth: 3,
-                    hoverBorderColor: '#000',
-                    hoverBackgroundColor: 'Red'
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
+        const chart = createChart();
+
+        function createChart() {
+            return new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: months,
+                    datasets: [{
+                        label: 'income',
+                        data: incomes,
+                        backgroundColor: '#0A0',
+                        borderWidth: 2,
+                        borderColor: '#777',
+                        hoverBorderWidth: 3,
+                        hoverBorderColor: '#000',
+                        hoverBackgroundColor: '#0A0'
+                        },
+                        {
+                        label: 'expenses',
+                        data: expenses,
+                        backgroundColor: '#D00',
+                        borderWidth: 2,
+                        borderColor: '#777',
+                        hoverBorderWidth: 3,
+                        hoverBorderColor: '#000',
+                        hoverBackgroundColor: 'Red'
                     }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
                 }
-            }
-        });
+            });
+        }
 
         function toggleCheckbox(elem) {
-            console.log(elem.target.id);
-            chart.data.datasets[1].data[2] = 3456;
+            if (elem.srcElement.checked) {
+                categoriesSet.add(elem.srcElement.value);
+            } else {
+                categoriesSet.delete(elem.srcElement.value);
+            }
+            recalc();
+            // chart.data.datasets[1].data[2] = 3456;
             chart.update();
         }
 
-        function addCheckboxes(categories) {
+        function addFilters(categories) {
             let parentElement = document.getElementById("filters");
             for (var count in categories)
             {
@@ -74,8 +79,7 @@
         function createCheckbox(count, category) {
             let newCheckBox = document.createElement('input');
             newCheckBox.type = 'checkbox';
-            newCheckBox.name = 'cat' + count;
-            newCheckBox.id = '' + count;
+            newCheckBox.id = 'cat' + count;
             newCheckBox.value = category;
             newCheckBox.checked = true;
             newCheckBox.addEventListener('click', toggleCheckbox);
@@ -93,16 +97,34 @@
             let month = transaction.month;
             expenses[month] += transaction.amount;
             transaction.categories.forEach(element => {
-                categories.add(element);
+                categoriesSet.add(element);
             });
         }
 
-        function refresh() {
-            chart.data.datasets[1].data[2] = 3456;
+        function recalc() {
+            incomes = new Array(12).fill(0);
+            expenses = new Array(12).fill(0);
+            transactions.map(t => calcExpenses(t));
+            chart.data.datasets[1].data = expenses;
             chart.update();
         }
 
-        async function getApiData() {
+        function calcExpenses(transaction) {
+            for (let c of transaction.categories) {
+                if (categoriesSet.has(c)) {                 // if one of the categories is checked
+                    let month = transaction.month;
+                    expenses[month] += transaction.amount;  // add amount to expenses for that month
+                    console.log("adding " + transaction.amount + " to " + transaction.month + " with " + transaction.categories)
+                    return;
+                }
+            }
+        }
+
+        function refresh() {
+            chart.update();
+        }
+
+        async function getTransactionData() {
             const result = await fetch('/transactions.json');
             const json = await result.json();
             return json;
